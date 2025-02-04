3194eb581e93a7912b44acd3a3fd8e9d4937d9ad
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-02-05 00:26:19 +0330

    create new module for log in

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -16,6 +16,7 @@ import {
   handleDefaultNoteMode,
   handleSetAccessKey,
 } from "@/utils/handleLocalStorage";
+import handleLogin from "@/api/handleLogIn";
 
 const Login = () => {
   const { loading, setLoading, setAccessKey, setAppMode } =
@@ -46,30 +47,8 @@ const Login = () => {
     });
   };
 
-  // handle login action
-  const handleLogin = async (data: any) => {
-    try {
-      const res = await fetch("http://10.0.2.2:3000/login", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json",
-          "x-api-key":
-            "shYqiZ7vc4?QoiatSIOA9MHMxOsBW2Wckzc5GAsO3xvzkUVr/24zxssYdAOlta-5/lKBdOb0Q3hW7ClRsrgAX?kmQa8-o9qfpwUhP7v/CR8St!wO5VanxxjZ12gG2CHi",
-        },
-      });
 
-      if (!res.ok) {
-        const errorData = await res.json();
-        return errorData;
-      }
 
-      const success = await res.json();
-      return success;
-    } catch (error: any) {
-      console.log("Error:", error.message);
-    }
-  };
 
   // submit log in form
   const onSubmit = async (data: any) => {
