5368ad216489bcc415e9e2f392ebb448692b8e3a
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-02-06 02:35:05 +0330

    user api address in deleting note

--- app/(note)/index.tsx
+++ app/(note)/index.tsx
@@ -132,15 +132,18 @@ const Note = () => {
   //------------  cloud action handlers
   const handleDeleteCloudNote = async () => {
     try {
-      const res = await fetch(`http://10.0.2.2:3000/delete_note/${id}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          "x-api-key":
-            "shYqiZ7vc4?QoiatSIOA9MHMxOsBW2Wckzc5GAsO3xvzkUVr/24zxssYdAOlta-5/lKBdOb0Q3hW7ClRsrgAX?kmQa8-o9qfpwUhP7v/CR8St!wO5VanxxjZ12gG2CHi",
-          Authorization: `Bearer ${accessKey}`,
-        },
-      });
+      const res = await fetch(
+        `https://myqnoteapi.liara.run/delete_note/${id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key":
+              "shYqiZ7vc4?QoiatSIOA9MHMxOsBW2Wckzc5GAsO3xvzkUVr/24zxssYdAOlta-5/lKBdOb0Q3hW7ClRsrgAX?kmQa8-o9qfpwUhP7v/CR8St!wO5VanxxjZ12gG2CHi",
+            Authorization: `Bearer ${accessKey}`,
+          },
+        }
+      );
       setUserNotes(await handleGetUserCloudNotes(accessKey));
       showToast("با موفقیت حذف شد");
       router.replace("/(home)");
