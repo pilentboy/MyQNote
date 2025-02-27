2f7332b93c75071ecb82cc1756deb4307ab5c661
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-02-28 02:22:50 +0330

    handle copying online note to local

--- api/handleAddUserCloudNote.ts
+++ api/handleAddUserCloudNote.ts
@@ -18,7 +18,7 @@ const handleAddingCloudNotes = async (
       "Content-Type": "application/json",
       "x-api-key":
         "shYqiZ7vc4?QoiatSIOA9MHMxOsBW2Wckzc5GAsO3xvzkUVr/24zxssYdAOlta-5/lKBdOb0Q3hW7ClRsrgAX?kmQa8-o9qfpwUhP7v/CR8St!wO5VanxxjZ12gG2CHi",
-      Authorization: `Bearer ${accessKey}`,
+      Authorization: `Bearer ${22}`,
     },
   });
 export default handleAddingCloudNotes;
